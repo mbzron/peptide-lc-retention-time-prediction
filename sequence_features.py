10d77b31cd4e11602ecb30ec81d0bdf46b2dd75d
mbzron <rojnorman@gmail.com>
2025-08-11 19:59:37 -0500

    one hot enc. and lin reg. gets MAE of ~25

--- sequence_features.py
+++ sequence_features.py
@@ -3,25 +3,27 @@ import numpy as np
 
 aa_list = list("ACDEFGHIKLMNPQRSTVWY")
 
-def one_hot_encode(seq):
-    mapping = {aa:i for i, aa in enumerate(aa_list)}
-    arr = np.zeros((len(seq), len(aa_list)), dtype=int)
-    for i, aa in enumerate(seq):
-        arr[i, mapping[aa]] = 1
-    return arr.flatten()
-
 def aa_composition(seq):
     counts = [seq.count(aa) / len(seq) for aa in aa_list]
     return np.array(counts)
 
+def one_hot_encode(seq, max_seq_len):
+    mapping = {aa:i for i, aa in enumerate(aa_list)}
+    arr = np.zeros((max_seq_len, len(aa_list)), dtype=int)
+    for i, aa in enumerate(seq):
+        arr[i, mapping[aa]] = 1
+    return arr.flatten()
+
 class SeqFeatures:
     def __init__(self, seqs):
         self.seqs = seqs
+        self._max_seq_len = max(len(s) for s in seqs)
 
     @property
     def one_hot_encode(self):
-        return np.array([one_hot_encode(s) for s in self.seqs])
+        return np.array([one_hot_encode(s, self._max_seq_len) for s in self.seqs])
     
     @property
     def aa_composition(self):
         return np.array([aa_composition(s) for s in self.seqs])
+    
